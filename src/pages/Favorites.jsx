e1855642b7ac9e6401ccd605b8e8ccd1e445a953
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-02-03 18:20:19 -0300

    requisito 1 - ok

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">
+        <p>Favorites Page</p>
+      </div>
+    );
+  }
+}
+
+export default Favorites;
