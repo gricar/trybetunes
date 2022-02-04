6f064a21386714d654868d622ceba62dc5534449
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-02-04 18:14:15 -0300

    requisito 3 - ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  state = {
+    userLogged: '',
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    this.getUserName();
+  }
+
   getUserName = async () => {
-    const responseName = await getUser();
-    console.log(responseName);
+    const { name } = await getUser();
+    if (name) this.setState({ userLogged: name, isLoading: false });
   }
 
   render() {
+    const { userLogged, isLoading } = this.state;
     return (
       <header data-testid="header-component">
         <h3>Header Page</h3>
-        <p data-testid="header-user-name">{ this.getUserName }</p>
+        {
+          isLoading
+            ? <Loading />
+            : <p data-testid="header-user-name">{ `Olá, ${userLogged}!` }</p>
+        }
       </header>
     );
   }
