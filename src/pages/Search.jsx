94ab2da6e2aac89e12ee2e3abc897871e90c5994
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-02-04 18:28:29 -0300

    requisito 5 - ok

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,47 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    searchInput: '',
+    isSearchButtonDisabled: true,
+  };
+
+  onSearchInputChange = ({ target }) => {
+    const { name, value } = target;
+    const minSearchInput = 2;
+
+    this.setState({
+      [name]: value,
+      isSearchButtonDisabled: value.length < minSearchInput,
+    });
+  }
+
   render() {
+    const { searchInput, isSearchButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search Page</p>
+        <form>
+          <label htmlFor="searchInput">
+            <input
+              type="text"
+              id="searchInput"
+              name="searchInput"
+              placeholder="Pesquise por uma música ou banda"
+              data-testid="search-artist-input"
+              value={ searchInput }
+              onChange={ this.onSearchInputChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isSearchButtonDisabled }
+            // onClick={ this. }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
