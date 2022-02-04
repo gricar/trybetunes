e0f784bf1d8fc8f54d07cce9db0354167768ae7d
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-02-03 21:12:26 -0300

    requisito 2 - ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,18 +7,64 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import Loading from './pages/Loading';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  state = {
+    userName: '',
+    isLoginButtonDisabled: true,
+    isLoading: false,
+    dataReceveid: false,
+  };
+
+  onUserInputChange = ({ target }) => {
+    const { name, value } = target;
+    const minUserName = 3;
+
+    this.setState({
+      [name]: value,
+      isLoginButtonDisabled: value.length < minUserName,
+    });
+  }
+
+  loginBtn = async () => {
+    this.setState({ isLoading: true });
+    const { userName } = this.state;
+    const response = await createUser({ name: userName });
+    if (response) {
+      this.setState({
+        isLoading: false,
+        dataReceveid: true,
+      });
+    }
+  }
+
   render() {
+    const { userName, isLoginButtonDisabled, isLoading, dataReceveid } = this.state;
+    const props = { userName, isLoginButtonDisabled };
+
+    if (isLoading) return <Loading />;
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ Login } />
+          { dataReceveid ? <Redirect to="/search" /> : null }
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <Login
+                { ...props }
+                onUserInputChange={ this.onUserInputChange }
+                isLoading={ this.loginBtn }
+              />
+            ) }
+          />
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/profile" component={ Profile } />
+          <Route exact path="/profile" component={ Profile } />
           <Route path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   render() {
+    const { userName, isLoginButtonDisabled, onUserInputChange, isLoading } = this.props;
     return (
       <div data-testid="page-login">
-        <p>Login Page</p>
+        <form>
+          <label htmlFor="userName">
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              placeholder="Digite o seu nome..."
+              data-testid="login-name-input"
+              value={ userName }
+              onChange={ onUserInputChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isLoginButtonDisabled }
+            onClick={ isLoading }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
 
 export default Login;
+
+const { string, func, bool } = PropTypes;
+
+Login.propTypes = {
+  userName: string.isRequired,
+  isLoginButtonDisabled: bool.isRequired,
+  onUserInputChange: func.isRequired,
+  isLoading: func.isRequired,
+};
