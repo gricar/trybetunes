cebe1b2b592284c7c580223e127c1222faa6c365
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-02-08 18:21:32 -0300

    requisito 8 - ok

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class MusicCard extends Component {
   render() {
-    const { music: { trackId, trackName, previewUrl } } = this.props;
+    const { music: { trackId, trackName, previewUrl }, onClick } = this.props;
     return (
       <div className="musicPlayer">
         <h3>{ trackName }</h3>
@@ -11,14 +11,15 @@ class MusicCard extends Component {
           <track kind="captions" />
           O seu navegador não suporta o elemento audio.
         </audio>
-        {/* <label htmlFor={ trackId }>
+        <label htmlFor={ trackId }>
           Favorita
           <input
             type="checkbox"
             id={ trackId }
             data-testid={ `checkbox-music-${trackId}` }
+            onClick={ onClick }
           />
-        </label> */}
+        </label>
       </div>
     );
   }
@@ -27,6 +28,7 @@ class MusicCard extends Component {
 export default MusicCard;
 
 MusicCard.propTypes = {
+  onClick: PropTypes.string.isRequired,
   music: PropTypes.shape({
     trackId: PropTypes.number,
     trackName: PropTypes.string,
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
-import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
+import Loading from './Loading';
+import getMusics from '../services/musicsAPI';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class Album extends Component {
   state = {
     artistName: '',
     collectionName: '',
+    imgUrl: '',
     listOfMusics: [],
+    loading: false,
   }
 
   componentDidMount() {
@@ -22,24 +26,41 @@ class Album extends Component {
     this.setState({
       artistName: musics[0].artistName,
       collectionName: musics[0].collectionName,
+      imgUrl: musics[0].artworkUrl100,
       listOfMusics: musicsList,
     });
   }
 
+  addFavSong = async ({ target }) => {
+    this.setState({ loading: true });
+
+    const { checked, id } = target;
+    const { listOfMusics } = this.state;
+    if (checked) {
+      const musicChecked = listOfMusics.find(({ trackId }) => trackId === Number(id));
+      await addSong(musicChecked);
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
-    const { artistName, collectionName, listOfMusics } = this.state;
+    const { artistName, collectionName, imgUrl, listOfMusics, loading } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
         <div>
+          {
+            loading && <Loading />
+          }
           <h3 data-testid="artist-name">{ artistName }</h3>
           <h3 data-testid="album-name">{ collectionName }</h3>
-          {/* <img src={ artworkUrl100 } alt={ artistName } /> */}
+          <img src={ imgUrl } alt={ artistName } />
           {
             listOfMusics.map((music) => (
               <MusicCard
                 key={ music.trackName }
                 music={ music }
+                onClick={ (event) => this.addFavSong(event) }
               />
             ))
           }
